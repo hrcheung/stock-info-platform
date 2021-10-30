9a7daedd25688ee67ad4e912481e65ec74f8c909
hzhang02 <zhang.haoran1@northeastern.edu>
2021-10-30 11:57:53 -0700

    hardcode API

--- index.js
+++ index.js
@@ -2,18 +2,46 @@ const express = require('express') //pull something to my computer
 const app = express()   // name it as app
 const exphbs = require('express-handlebars');
 const path = require("path");
+const request = require("request");
 
 const PORT = process.env.PORT || 5000; //use theirs or use 5000
+
+
+//API key pk_8ea99c28ba4444ab94f489185aeccca1 
+// create a function that maps the output
+function call_api(finishedAPI){
+    request('https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_8ea99c28ba4444ab94f489185aeccca1',{json:true},(err,res,body)=>{
+        if(err){return console.log(err);}
+        if (res.statusCode===200){
+            finishedAPI(body);
+            };
+
+    });
+};
+
+
+
 
 //set handlebars middleware
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
+const otherstuff="company-detail";
+
 //Set handlebar routes
 app.get('/', function (req, res) {
-    res.render('home');
+    call_api(function(doneAPI){
+            res.render('home',{  //pass variables
+            stock:doneAPI
+            });
+    });
+    
 });
 
+//create about page route
+app.get('/about.html', function (req, res) {
+    res.render('about');
+});
 
 //Set static folder
 app.use(express.static(path.join(__dirname,"public")));
