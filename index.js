2c88d84ae28083d0cbd0a69448cfe2d3be4a1d3d
hzhang02 <zhang.haoran1@northeastern.edu>
2021-11-05 23:49:22 -0700

    submission

--- index.js
+++ index.js
@@ -3,22 +3,25 @@ const app = express()   // name it as app
 const exphbs = require('express-handlebars');
 const path = require("path");
 const request = require("request");
+const bodyParser=require("body-parser");
 
 const PORT = process.env.PORT || 5000; //use theirs or use 5000
 
 
 //API key pk_8ea99c28ba4444ab94f489185aeccca1 
 // create a function that maps the output
-function call_api(finishedAPI){
+function call_api(finishedAPI,ticker){
     request('https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_8ea99c28ba4444ab94f489185aeccca1',{json:true},(err,res,body)=>{
         if(err){return console.log(err);}
         if (res.statusCode===200){
             finishedAPI(body);
             };
-
     });
 };
 
+
+//use body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
 
 
 
@@ -28,13 +31,25 @@ app.set('view engine', 'handlebars');
 
 const otherstuff="company-detail";
 
-//Set handlebar routes
+//Set handlebar index GET routes
 app.get('/', function (req, res) {
     call_api(function(doneAPI){
             res.render('home',{  //pass variables
             stock:doneAPI
             });
     });
+    
+});
+
+//call_api(function,req.body.stock_ticker)
+//Set handlebar index POST routes
+app.post('/', function (req, res) {
+    call_api(function(doneAPI){
+        //posted_stuff=req.body.stock_ticker;
+            res.render('home',{  //pass variables
+            stock:doneAPI,
+            });
+    },req.body.stock_ticker);
     
 });
 
